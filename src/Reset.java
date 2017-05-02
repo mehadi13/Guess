7ef0369f78a4c75b849f95df10ea01530f09cae0
Mehadi Hossain <mehadi_hossain@yahoo.com>
2017-05-02 20:04:19 +0600

    Initial commit

---
+++ src/Reset.java
@@ -0,0 +1,17 @@
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mehadi hossain
+ */
+public class Reset {
+      void reset()
+    {
+        new play2().setVisible(true);
+    }
+}
